1c14984b2e6feccabd2ab0939a4d512269989061
gitbeet <nikig715@gmail.com>
2025-02-09 14:43:10 +0100

    bubble sort

---
+++ sort/bubbleSort.js
@@ -0,0 +1,17 @@
+const bubbleSort = (arr) => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let swapped = false;
+    for (let j = 0; j < i; j++) {
+      if (arr[j + 1] < arr[j]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swapped = true;
+      }
+    }
+    if (!swapped) break;
+  }
+  return arr;
+};
+
+console.log(bubbleSort([5, 4, 3, 6, 7, 12, 1, 0]));
