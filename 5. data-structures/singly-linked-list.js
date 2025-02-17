2cdb8a6908dcdf678454f298067473c1130ec5f0
gitbeet <nikig715@gmail.com>
2025-02-17 19:42:40 +0100

    sll classes, push method

---
+++ 5. data-structures/singly-linked-list.js
@@ -0,0 +1,30 @@
+class Node {
+  constructor(value) {
+    this.val = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const sll = new SinglyLinkedList();
+console.log(sll.push(2));
+console.log(sll.push(3));
