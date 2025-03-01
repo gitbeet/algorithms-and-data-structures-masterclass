29e7170ae4671dcf64789a6b9ba2aeb387dc1ed1
gitbeet <nikig715@gmail.com>
2025-03-01 10:27:51 +0100

    weighted graph class

---
+++ 5. data-structures/weighted-graph.js
@@ -0,0 +1,35 @@
+class WeightedGraph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+  addEdge(vertex1, vertex2, weight) {
+    this.adjacencyList[vertex1].push({ node: vertex2, weight });
+    this.adjacencyList[vertex2].push({ node: vertex1, weight });
+  }
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v.node !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v.node !== vertex1
+    );
+  }
+  removeVertex(vertex) {
+    this.adjacencyList[vertex].forEach((v) => {
+      this.removeEdge(vertex, v.node);
+    });
+    delete this.adjacencyList[vertex];
+  }
+}
+
+const graph = new WeightedGraph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addEdge("A", "B", 123);
+graph.removeVertex("A");
+console.log(graph.adjacencyList);
